4edea30ccdb732047c689ad0ced1a3f2d53b0e7b
Risher Randall <randallr18@mail.wlu.edu>
2018-07-06 15:20:31 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,25 @@
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# #   Mayor.create(name: 'Emanuel', city: cities.first)
+# create_table "artists", force: :cascade do |t|
+#   t.string   "name"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+# end
+#
+# create_table "songs", force: :cascade do |t|
+#   t.string   "title"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+#   t.integer  "artist_id"
+# end
+artist1 = Artist.create(name: 'Drake')
+artist2 = Artist.create(name: 'Van Morrison')
+artist3 = Artist.create(name: 'Michael Jackson')
+artist4 = Artist.create(name: 'Bob Dylan')
+
+song1 = Song.create(title: 'Brown Eyed Girl', artist_id: 2)
+song2 = Song.create(title: 'Believe Me', artist_id: 1)
+song3 = Song.create(title: 'Gloria', artist_id: 2)
+song4 = Song.create(title: 'Rolling Stone', artist_id: 4)
